2db9b8a88bee4a348c7826537bdcde049b2e338e
CI <irsooti+ci@gmail.com>
2018-11-27 14:28:18 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b8d7635023aac4e85f80df6c81ce2b0.js"
+  "/precache-manifest.5d522d319aea60ddbe9d84371f50fa1b.js"
 );
 
 workbox.clientsClaim();
